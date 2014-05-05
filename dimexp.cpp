8aa9e40c10eb937be2f248ccd4e43d1e497690c9
joryschossau <jory@msu.edu>
2014-05-05 14:47:49 -0400

    initial commit

---
+++ dimexp.cpp
@@ -0,0 +1,155 @@
+//description: converts 1D index into higher D indices (0-based indices)
+//output: space separated list of dimension indices large dim to small
+//input: <1D index integer> <list of dimension sizes until n-1>...
+//example:
+        //input: dimexp 115 10
+        //output: 11 5
+
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+void showHelp(char* progname);
+
+using namespace std;
+
+bool getRangePair(const char* token, char lower[], char upper[]) {
+	char* pos = (char*)strpbrk(token, ":");
+	if (pos == NULL) {
+		upper=strcpy(upper,token);
+		upper[strlen(token)]='\0';
+		lower[0]='\0';
+		return false;
+	}
+	strncpy(lower, token, pos-token);
+	lower[pos-token]='\0';
+	strcpy(upper, pos+1);
+	upper[strlen(pos)]='\0';
+	if (strlen(upper) == 0)
+		return false;
+	return true;
+}
+
+struct triple{
+	int first, second, third;
+	triple(int one, int two, int three): first(one), second(two), third(three) {}
+};
+
+
+int main (int argc, char**argv) {
+
+	unsigned int index = 0;
+	unsigned int maxSize = 0;
+	char lower[16];
+	char upper[16];
+	vector<triple> ranges;
+	int indexToExtract = 0;
+
+	if (argc<3) {
+		showHelp(argv[0]);
+		return 0;
+	}
+
+	bool queriedForTotal=false; // true if user arguments: ? #:# #:# ...
+
+	if ((strcmp(argv[1],"q") == 0) || (strcmp(argv[1],"?") == 0))
+		queriedForTotal=true;
+	else
+		index = atoi(argv[1]);
+
+	for (int i=2; i<argc; i++) {
+		if (getRangePair(argv[i], lower, upper)) {
+			if (atoi(upper) >= atoi(lower)) {
+			//cout << "[" << lower << ":" << upper << "]" << endl;
+			ranges.push_back(triple(atoi(lower), atoi(upper), atoi(upper)+1-atoi(lower)));
+			} else {
+				cout << "warning: range backwards? argument " << i << ": '" << argv[i] << "'" << endl;
+				cout << "\tassuming you meant " << upper << ":" << lower << endl;
+				//cout << "[" << upper << ":" << lower << "]" << endl;
+				//ranges.push_back(make_pair(atoi(upper), atoi(lower)));
+			}
+		} else {
+			if (strlen(lower)==0) {
+				indexToExtract = atoi(upper);
+			} else
+				cout << "input error with argument " << i << ": '" << argv[i] << "'" << endl;
+		}
+	}
+
+	maxSize=1;
+	for (auto &p: ranges) {
+		maxSize*=(p.third);
+	}
+	if (queriedForTotal) {
+		cout << maxSize << endl;
+	} else { // user wants the indices or an index
+		vector<int> result;
+		vector<int> resultReversed;
+		//stringstream result;
+		int dynDimUnit=0;
+		int dimIndex=0;
+		int rem=index-1;
+
+		if ((index < 1) || (index > maxSize)) {
+			cout << "error: index '" << index << "' out of range ('1:" << maxSize << "')" << endl;
+			return 1;
+		}
+
+		for (int n=ranges.size()-1; n>=0; --n) {
+			dynDimUnit=1;
+			for (int v=n; v>=0; --v) {
+				dynDimUnit*=ranges[v].third;
+			}
+			dimIndex=rem/dynDimUnit;
+			result.push_back(dimIndex);
+			rem=rem-dynDimUnit*dimIndex;
+		}
+		result.push_back(rem);
+
+		resultReversed.resize(result.size()-1);
+		reverse_copy(result.begin()+1, result.end(), resultReversed.begin());
+		
+		if (indexToExtract > 0)
+			if (indexToExtract < argc-2)
+				cout << resultReversed[indexToExtract-1]+ranges[indexToExtract-1].first;
+			else
+				cout << "error: target index '" << indexToExtract << "' out of range '1:" << argc-3 << "'" << endl;
+		else {
+			for (int i=0; i< resultReversed.size(); i++)
+				cout << resultReversed[i]+ranges[i].first << " ";
+			cout << endl;
+		}
+	}
+	return 0;
+}
+
+void showHelp(char* progname) {
+	cout << endl;
+	cout << "\tDescription: Expands a 1-dimensional number into" << endl;
+	cout << "\tan n-dimensional number given specific ranges and an index." << endl;
+	cout << "\tindex is 0-based and all numbers are inclusive." << endl;
+	cout << "\t useful for performing sweeps of all combinations." << endl;
+	cout << endl;
+	cout << "\tUsage: " << uppercase << progname << nouppercase
+		<< " index start:end start:end start:end..." << endl;
+	cout << endl;
+	cout << "\tExamples: " << endl;
+	cout << "\tInput: " <<  uppercase << progname << nouppercase
+		<< " 15 1:9 8:10" << endl;
+	cout << "\tOutput: 5 10" << endl;
+	cout << endl;
+	cout << "\tInput: " << uppercase << progname << nouppercase
+		<< " 52 8:10 1:9" << endl;
+	cout << "\tOutput: 0 0" << endl;
+	cout << "\t(index out of range)" << endl;
+	cout << endl;
+	cout << "\tInput: " << uppercase << progname << nouppercase
+		<< " ? 8:10 1:9" << endl;
+	cout << "\tOutput: 27" << endl;
+	cout << "\t(number of valid indices)" << endl;
+	cout << endl;
+}
---
+++ queue.py
@@ -0,0 +1,245 @@
+#!/mnt/home/jory/epd/bin/python
+# path to python
+
+# where is your writable directory? Jobs will be managed in a .queue directory here.
+SCRATCH="/mnt/scratch/schossau"
+
+# username
+USER="jory"
+
+# how big is one batch of jobs? ex 10 means there must be 10 free slots to run another batch.
+JOB_ARRAY_MAX=10
+
+# max total jobs to run in parallel with this tool (ideally multiple of JOB_ARRAY_MAX)
+QUEUE_MAX=200
+
+##
+##
+## Customize everything before this line
+##
+##
+
+import os, sys, shutil
+import glob
+import pickle
+import time
+import re
+from subprocess import check_output
+import argparse
+
+basedir = os.path.dirname(os.path.realpath(__file__))
+
+def joinpath(*args):
+	build = args[0]
+	for iarg in range(1,len(args)):
+		build = os.path.join(build,args[iarg]);
+	return build;
+
+def existingProjects():
+	projects = [];
+	confdir = joinpath(SCRATCH, ".queue");
+	#print("checking if "+confdir+" is a dir");
+	if os.path.isdir(confdir):
+		#print("yes, hidden dir exists");
+		for folder in os.walk(confdir).next()[1]:
+			#print("found folder: "+folder);
+			projectdir = joinpath(confdir,folder);
+			projects.append(projectdir);
+		return projects
+	else:
+		#print("nope, "+confdir+" is not a dir");
+		return False
+
+def quitWithMsg(msg):
+	print("\n");
+	print(msg);
+	sys.exit(1);
+
+def runningJobs():
+	'''Returns list of job ids (just the 12345 number part)
+	for all running jobs of the specified user'''
+
+	list_running = check_output(["qstat -au "+USER+" | tail -n +6 | cut -d'.' -f1 | cut -d'[' -f1"], shell=True);
+	list_running = list_running.split('\n');
+	list_running = [int(e) for e in list_running if len(e) != 0];
+	return list_running;
+
+def splitArrayJob(jobfile,jobtext,begin,end):
+	begin = int(begin);
+	end = int(end);
+	rxBegin = re.compile(r'''XBEGINX''');
+	rxEnd = re.compile(r'''XENDX''');
+	SETS = (end - begin + 1) / JOB_ARRAY_MAX; #SETS=`echo "($END - $BEGIN + 1) / $JOB_ARRAY_MAX" | bc`
+	REMAINDER = (end - begin + 1) % JOB_ARRAY_MAX; #REMAINDER=`echo "($END - $BEGIN + 1) % $JOB_ARRAY_MAX" | bc`
+	firstJobMade=False;
+	projectdir=joinpath(SCRATCH,".queue");
+	jobs_to_add = [];
+	## create output directory
+	projdirname = "array_"+jobfile;
+	if not os.path.exists(joinpath(SCRATCH,".queue",projdirname)):
+		os.makedirs(joinpath(SCRATCH,".queue",projdirname));
+	for set in range(SETS): #for set in $(seq 0 $[SETS-1]); do
+		this_filename = str(set*JOB_ARRAY_MAX+begin)+"."+str(set*JOB_ARRAY_MAX+begin+JOB_ARRAY_MAX-1)+".sh"; #THIS_FILENAME=$[set*JOB_ARRAY_MAX+BEGIN].$[set*JOB_ARRAY_MAX+BEGIN+JOB_ARRAY_MAX-1].sh
+		jobs_to_add.append(this_filename);
+		open(joinpath(projectdir,"array_"+jobfile,this_filename),'w').write(rxEnd.sub(str(set*JOB_ARRAY_MAX+begin+JOB_ARRAY_MAX-1),rxBegin.sub(str(set*JOB_ARRAY_MAX+begin),jobtext)));
+		#check_output(["sed -e s#XBEGINX#"+str(set*JOB_ARRAY_MAX+begin)+"#g "+jobfile+" | sed -e s#XENDX#"+str(set*JOB_ARRAY_MAX+begin+JOB_ARRAY_MAX-1)+"#g > "+joinpath(projectdir,"array_"+jobfile,this_filename)], shell=True); #sed -e s#XBEGINX#$[set*JOB_ARRAY_MAX+BEGIN]#g $FILE | sed -e s#XENDX#$[set*JOB_ARRAY_MAX+BEGIN+JOB_ARRAY_MAX-1]#g > array_$FILE/$THIS_FILENAME
+		firstJobMade=True;
+	if (REMAINDER != 0): #if ! [ "$REMAINDER" -eq 0 ]; then #if there is an incomplete set (< JOB_ARRAY_MAX) then submit those
+		this_filename = str(SETS*JOB_ARRAY_MAX+begin)+"."+str(end)+".sh"; #THIS_FILENAME=$[SETS*JOB_ARRAY_MAX+BEGIN].$END.sh
+		jobs_to_add.append(this_filename);
+		if (firstJobMade == True): #if [ -z $PREV_JOBID ]; then #if this is first job and there is only an incomplete set (< JOB_ARRAY_MAX)
+			open(joinpath(projectdir,"array_"+jobfile,this_filename),'w').write(rxEnd.sub(str(end),rxBegin.sub(str(SETS*JOB_ARRAY_MAX+begin),jobtext)));
+			#check_output(["sed -e s#XBEGINX#"+str(SETS*JOB_ARRAY_MAX+begin)+"#g "+jobfile+" | sed -e s#XENDX#"+str(end)+"#g > "+joinpath(projectdir,"array_"+jobfile,this_filename)], shell=True); #sed -e s#XBEGINX#$[SETS*JOB_ARRAY_MAX+BEGIN]#g $FILE | sed -e s#XENDX#$END#g > array_$FILE/$THIS_FILENAME
+		else: #else #if this job has predecessors and this job is an incompelte set (< JOB_ARRAY_MAX)
+			open(joinpath(projectdir,"array_"+jobfile,this_filename),'w').write(rxEnd.sub(str(end),rxBegin.sub(str(SETS*JOB_ARRAY_MAX+begin),jobtext)));
+			#check_output(["sed -e s#XBEGINX#"+str(SETS*JOB_ARRAY_MAX+begin)+"#g "+jobfile+" | sed -e s#XENDX#"+str(end)+"#g > "+joinpath(projectdir,"array_"+jobfile,this_filename)], shell=True); #sed -e s#XBEGINX#$[set*JOB_ARRAY_MAX+BEGIN]#g $FILE | sed -e s#XENDX#$[set*JOB_ARRAY_MAX+BEGIN+JOB_ARRAY_MAX-1]#g > array_$FILE/$THIS_FILENAME
+	pickle.dump(jobs_to_add, open(joinpath(SCRATCH,".queue",projdirname,".held"),"wb"));
+	submitted = [];
+	pickle.dump(submitted, open(joinpath(SCRATCH,".queue",projdirname,".submitted"),"wb")); #write empty array so file exists
+
+def checkOnJobsForProjects():
+	"""Submits jobs for folders found in the .queue hidden folder.
+	Returns False if there were jobs found to submit or running, True otherwise."""
+	
+	projects = existingProjects();
+	if (projects == False):
+		quitWithMsg("No projects found\n");
+	running_jobs = runningJobs();
+	available_slots = 0; #how many jobs we can submit at the end of evaluation
+	zeroJobsLeft = True;
+	for project in projects:
+		submitted_jobs = pickle.load(open(joinpath(project,".submitted"),"rb"));
+		held_jobs = pickle.load(open(joinpath(project,".held"),"rb"));
+		for job in submitted_jobs:
+			if (job not in running_jobs):
+				submitted_jobs.remove(job);
+		if (len(held_jobs) == 0 and len(submitted_jobs) == 0):
+			shutil.rmtree(project) #remove finished project
+			continue;
+		else:
+			zeroJobsLeft = False;
+		available = QUEUE_MAX - (len(running_jobs)*JOB_ARRAY_MAX);
+		if (available >= 0):
+			available_slots += (available/JOB_ARRAY_MAX);
+		while ((available_slots > 0) and (len(held_jobs) > 0)):
+			job = held_jobs.pop();
+			jobID = submitJobGetID(joinpath(project,job));
+			submitted_jobs.append(jobID);
+			available_slots -= 1;
+		pickle.dump(submitted_jobs, open(joinpath(project,".submitted"),"wb"));
+		pickle.dump(held_jobs, open(joinpath(project,".held"),"wb"));
+	return zeroJobsLeft;
+
+def submitJobGetID(jobFileFullPath):
+	'''Submits a job file given the full path
+	and returns the integer job id part of 12345.job.mgr[] etc.'''
+
+	ID = check_output(["qsub "+jobFileFullPath], shell=True); 
+	ID = int(ID.split('\n')[0].split('.')[0].split('[')[0]);
+	return ID;
+
+def daemonIsRunning():
+	daemonfile = joinpath(SCRATCH,'.queue','.daemon');
+	if os.path.exists(daemonfile) and os.path.isfile(daemonfile):
+		return True;
+	else:
+		return False;
+
+def markDaemonIsRunning():
+	daemonfile = joinpath(SCRATCH,'.queue','.daemon');
+	open(daemonfile, 'a').close();
+
+def markDaemonNotRunning():
+	daemonfile = joinpath(SCRATCH,'.queue','.daemon');
+	if os.path.exists(daemonfile) and os.path.isfile(daemonfile):
+		os.remove(daemonfile);
+
+def getArrayRangeAndTemplateFile(filename):
+	'''takes a filename of a qsub submit file.
+	returns the numbers used in this type of line: #PBS -t 1-100
+	and returns the new file contents replaced with template symbols: #PBS -t XBEGINX-XENDX'''
+
+	filetext = None;
+	with open(filename, 'r') as filehandle:
+		filetext = filehandle.read();
+	regex = re.compile(r'''PBS -t[\s]*(\d+)[\s]*-[\s]*(\d+)''');
+	match = regex.search(filetext);
+	filetext = regex.sub(r'''PBS -t XBEGINX-XENDX''',filetext);
+	return (match.group(1), match.group(2), filetext);
+
+def submitSelf():
+	check_output(["qsub -j oe -o /dev/null -l mem=2gb,nodes=1:ppn=1,walltime=03:59:59 "+joinpath(SCRATCH,'.queue','queue.py')], shell=True);
+
+def help():
+	'''Returns args object.'''
+
+	print("  Usage: ./queue.py [qsub file or resume]")
+	print("")
+	print("  qsub file: A qsub array job.")
+	print("  resume: Resubmits self in job manager mode if necessary.")
+	print("  <no arguments>: Assumes run in automated job manager mode")
+	print("    and will kill itself and resubmit itself every 4 hours.")
+	print("")
+	print("  Warning: this script possibly produces many files.")
+	print("    take care to remove job directories no longer")
+	print("    needed by the end of the qsub file.")
+	print("")
+	print("  The working folder is "+joinpath(SCRATCH,".queue")+" which")
+	print("  contains a semaphore .daemon file, and a project directory")
+	print("  for each submitted qsub array file using this tool.")
+	print("  Each project directory contains prepared qsub files for")
+	print("  each smaller array segment, as well as two hidden files")
+	print("  .held and .submitted. .held is a list of qsub files yet")
+	print("  to be submitted. .submitted contains job ids for jobs")
+	print("  that are running.")
+	print("")
+	print("  Workflow: submit a qsub array job. queue.py is automatically")
+	print("  submitted as a 4 hour job which every minute checks if job")
+	print("  status has changed. If so then submits a new chunk of jobs")
+	print("  if there is room. A .daemon file is created at the beginning")
+	print("  of the job manager 4 hour run, and removed at the end.")
+	print("  This .daemon file helps prevent multiple job managers running.")
+	print("  However, if you kill the job manager, simpley ./queue.py resume")
+	print("  and the job manager will be forced into a running state after")
+	print("  submission. The project directories in")
+	print("  "+joinpath(SCRATCH,".queue")+" are each removed after")
+	print("  all jobs in them are completed.");
+	print("")
+
+def main():
+	if (len(sys.argv) > 1): # Users wants to submit a new job
+		if (sys.argv[1] in ["-h","--h","--help","-?","--?"]):
+			help()
+			sys.exit(0);
+		if (sys.argv[1] == "resume"):
+			markDaemonIsRunning();
+			shutil.copy("queue.py", joinpath(SCRATCH,'.queue','queue.py'));
+			submitSelf();
+			sys.exit(0);
+		rangeBegin, rangeEnd, fileTemplated = getArrayRangeAndTemplateFile(sys.argv[1]);
+		splitArrayJob(sys.argv[1], fileTemplated, rangeBegin, rangeEnd);
+		shutil.copy("queue.py", joinpath(SCRATCH,'.queue','queue.py'));
+		checkOnJobsForProjects();
+		if not daemonIsRunning():
+			markDaemonIsRunning();
+			submitSelf();
+	else:							# Not user-mode, but automated startup to maintain jobs
+		if not daemonIsRunning():
+			sys.stderr.write("Something is wrong, because we're running a new instance but the daemon flag is gone. Shutting down.\n");
+			sys.exit(0);
+		justUnder4Hours = 3600*4 - 60*10; #10 minutes under
+		timeStart = time.time();
+		while ((time.time() - timeStart) < justUnder4Hours):
+			done = checkOnJobsForProjects();
+			if (done == True):
+				#print("we're all done here. Wrapping up.");
+				markDaemonNotRunning();
+				sys.exit(0);
+			else:
+				#print("let's wait another minute.");
+				time.sleep(60); # wait one minute
+		submitSelf();
+		sys.exit(0);
+
+if __name__ == "__main__":
+	main();
